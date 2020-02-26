10012e158ea668a358a672016418c6024de597e7
Vrndavana <Vrmdavama.th@gmail.com>
2020-02-26 15:31:04 -0800

    User & Password: User & Pass

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ function authenticator(req, res, next) {
 
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
-  if (username === 'V' && password === 'perswerd') {
+  if (username === 'User' && password === 'Pass') {
     req.loggedIn = true;
     res.status(200).json({
       payload: token
